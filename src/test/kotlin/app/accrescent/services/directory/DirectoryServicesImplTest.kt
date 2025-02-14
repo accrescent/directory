f8794699a8bf162482c9dc6cb33108c8f39dc64f
lberrymage <logan@lberrymage.dev>
2025-02-14 10:36:52 -0700

    Move large test data protobufs to dedicated files

--- src/test/kotlin/app/accrescent/services/directory/DirectoryServicesImplTest.kt
+++ src/test/kotlin/app/accrescent/services/directory/DirectoryServicesImplTest.kt
@@ -304,51 +304,13 @@ class DirectoryServicesImplTest {
         .build()
 
     companion object {
-        private val validCreateAppRequest: CreateAppRequest = CreateAppRequest.newBuilder()
-            .setAppId("app.accrescent.client")
-            .setApp(
-                App.newBuilder()
-                    .setDefaultListingLanguage("en")
-                    .addAllListings(
-                        listOf(
-                            InternalAppListing.newBuilder()
-                                .setLanguage("en")
-                                .setName("Accrescent")
-                                .setShortDescription("A private and secure Android app store")
-                                .setIcon(
-                                    InternalImage.newBuilder()
-                                        .setObjectId("57297a7-6f2c-4a04-9656-497af21bf6b2")
-                                )
-                                .build(),
-                            InternalAppListing.newBuilder()
-                                .setLanguage("de")
-                                .setName("Accrescent")
-                                .setShortDescription("Ein privater und sicherer Android App Store")
-                                .setIcon(
-                                    InternalImage.newBuilder()
-                                        .setObjectId("57297a7-6f2c-4a04-9656-497af21bf6b2")
-                                )
-                                .build(),
-                        ),
-                    )
-                    .addPackageMetadata(
-                        PackageMetadataEntry.newBuilder()
-                            .setReleaseChannel(
-                                ReleaseChannel.newBuilder()
-                                    .setWellKnown(ReleaseChannel.WellKnown.WELL_KNOWN_STABLE)
-                            )
-                            .setPackageMetadata(
-                                javaClass.classLoader
-                                    .getResourceAsStream("accrescent-0.25.0-package-metadata.txtpb")
-                                    ?.use {
-                                        val builder = PackageMetadata.newBuilder()
-                                        it.reader().use { TextFormat.merge(it, builder) }
-                                        builder
-                                    }
-                            ),
-                    )
-                    .build()
-            )
+        private val validCreateAppRequest = javaClass.classLoader
+            .getResourceAsStream("valid-create-app-request.txtpb")!!
+            .use {
+                val builder = CreateAppRequest.newBuilder()
+                it.reader().use { TextFormat.merge(it, builder) }
+                builder
+            }
             .build()
 
         // Retrieved from an Android 15 Pixel 9 x86_64 emulator using `bundletool get-device-spec`
@@ -356,176 +318,13 @@ class DirectoryServicesImplTest {
         //
         // Has all known fields set as of bundletool 1.18.0 except device_tier, device_groups, and
         // country_set.
-        private val validDeviceAttributes = DeviceAttributes.newBuilder()
-            .setSpec(
-                Devices.DeviceSpec.newBuilder()
-                    .addAllSupportedAbis(listOf("x86_64", "arm64-v8a"))
-                    .addSupportedLocales("en-US")
-                    .addAllDeviceFeatures(
-                        listOf(
-                            "reqGlEsVersion=0x30000",
-                            "android.hardware.audio.output",
-                            "android.hardware.biometrics.face",
-                            "android.hardware.bluetooth",
-                            "android.hardware.bluetooth_le",
-                            "android.hardware.camera",
-                            "android.hardware.camera.any",
-                            "android.hardware.camera.autofocus",
-                            "android.hardware.camera.capability.manual_post_processing",
-                            "android.hardware.camera.capability.manual_sensor",
-                            "android.hardware.camera.capability.raw",
-                            "android.hardware.camera.concurrent",
-                            "android.hardware.camera.flash",
-                            "android.hardware.camera.front",
-                            "android.hardware.camera.level.full",
-                            "android.hardware.faketouch",
-                            "android.hardware.fingerprint",
-                            "android.hardware.hardware_keystore=300",
-                            "android.hardware.identity_credential=202301",
-                            "android.hardware.keystore.app_attest_key",
-                            "android.hardware.location",
-                            "android.hardware.location.gps",
-                            "android.hardware.location.network",
-                            "android.hardware.microphone",
-                            "android.hardware.ram.normal",
-                            "android.hardware.reboot_escrow",
-                            "android.hardware.screen.landscape",
-                            "android.hardware.screen.portrait",
-                            "android.hardware.security.model.compatible",
-                            "android.hardware.sensor.accelerometer",
-                            "android.hardware.sensor.ambient_temperature",
-                            "android.hardware.sensor.barometer",
-                            "android.hardware.sensor.compass",
-                            "android.hardware.sensor.gyroscope",
-                            "android.hardware.sensor.light",
-                            "android.hardware.sensor.proximity",
-                            "android.hardware.sensor.relative_humidity",
-                            "android.hardware.telephony",
-                            "android.hardware.telephony.calling",
-                            "android.hardware.telephony.data",
-                            "android.hardware.telephony.gsm",
-                            "android.hardware.telephony.ims",
-                            "android.hardware.telephony.messaging",
-                            "android.hardware.telephony.radio.access",
-                            "android.hardware.telephony.subscription",
-                            "android.hardware.touchscreen",
-                            "android.hardware.touchscreen.multitouch",
-                            "android.hardware.touchscreen.multitouch.distinct",
-                            "android.hardware.touchscreen.multitouch.jazzhand",
-                            "android.hardware.vulkan.compute",
-                            "android.hardware.vulkan.level=1",
-                            "android.hardware.vulkan.version=4206592",
-                            "android.hardware.wifi",
-                            "android.hardware.wifi.direct",
-                            "android.hardware.wifi.passpoint",
-                            "android.software.activities_on_secondary_displays",
-                            "android.software.adoptable_storage",
-                            "android.software.app_enumeration",
-                            "android.software.app_widgets",
-                            "android.software.autofill",
-                            "android.software.backup",
-                            "android.software.cant_save_state",
-                            "android.software.companion_device_setup",
-                            "android.software.controls",
-                            "android.software.credentials",
-                            "android.software.cts",
-                            "android.software.device_admin",
-                            "android.software.device_lock",
-                            "android.software.erofs",
-                            "android.software.file_based_encryption",
-                            "android.software.home_screen",
-                            "android.software.incremental_delivery=2",
-                            "android.software.input_methods",
-                            "android.software.ipsec_tunnel_migration",
-                            "android.software.ipsec_tunnels",
-                            "android.software.live_wallpaper",
-                            "android.software.managed_users",
-                            "android.software.midi",
-                            "android.software.opengles.deqp.level=132645633",
-                            "android.software.picture_in_picture",
-                            "android.software.print",
-                            "android.software.secure_lock_screen",
-                            "android.software.securely_removes_users",
-                            "android.software.telecom",
-                            "android.software.verified_boot",
-                            "android.software.virtualization_framework",
-                            "android.software.voice_recognizers",
-                            "android.software.vulkan.deqp.level=132645633",
-                            "android.software.webview",
-                            "android.software.window_magnification",
-                            "com.google.android.apps.dialer.SUPPORTED",
-                            "com.google.android.feature.EXCHANGE_6_2",
-                            "com.google.android.feature.GOOGLE_BUILD",
-                            "com.google.android.feature.GOOGLE_EXPERIENCE",
-                            "com.google.android.feature.PERSONAL_SAFETY",
-                            "com.google.android.feature.WELLBEING",
-                        ),
-                    )
-                    .addAllGlExtensions(
-                        listOf(
-                            "GL_EXT_debug_marker",
-                            "GL_EXT_robustness",
-                            "GL_OES_EGL_sync",
-                            "GL_OES_EGL_image",
-                            "GL_OES_EGL_image_external",
-                            "GL_OES_depth24",
-                            "GL_OES_depth32",
-                            "GL_OES_element_index_uint",
-                            "GL_OES_texture_float",
-                            "GL_OES_texture_float_linear",
-                            "GL_OES_compressed_paletted_texture",
-                            "GL_OES_compressed_ETC1_RGB8_texture",
-                            "GL_OES_depth_texture",
-                            "GL_OES_texture_half_float",
-                            "GL_OES_texture_half_float_linear",
-                            "GL_OES_packed_depth_stencil",
-                            "GL_OES_vertex_half_float",
-                            "GL_OES_standard_derivatives",
-                            "GL_OES_texture_npot",
-                            "GL_OES_rgb8_rgba8",
-                            "GL_EXT_color_buffer_float",
-                            "GL_EXT_color_buffer_half_float",
-                            "GL_EXT_texture_format_BGRA8888",
-                            "GL_APPLE_texture_format_BGRA8888",
-                            "ANDROID_EMU_CHECKSUM_HELPER_v1",
-                            "ANDROID_EMU_native_sync_v2",
-                            "ANDROID_EMU_dma_v1",
-                            "ANDROID_EMU_direct_mem",
-                            "ANDROID_EMU_host_composition_v1",
-                            "ANDROID_EMU_host_composition_v2",
-                            "ANDROID_EMU_vulkan",
-                            "ANDROID_EMU_deferred_vulkan_commands",
-                            "ANDROID_EMU_vulkan_null_optional_strings",
-                            "ANDROID_EMU_vulkan_create_resources_with_requirements",
-                            "ANDROID_EMU_YUV_Cache",
-                            "ANDROID_EMU_vulkan_ignored_handles",
-                            "ANDROID_EMU_has_shared_slots_host_memory_allocator",
-                            "ANDROID_EMU_vulkan_free_memory_sync",
-                            "ANDROID_EMU_vulkan_shader_float16_int8",
-                            "ANDROID_EMU_vulkan_async_queue_submit",
-                            "ANDROID_EMU_vulkan_queue_submit_with_commands",
-                            "ANDROID_EMU_sync_buffer_data",
-                            "ANDROID_EMU_vulkan_async_qsri",
-                            "ANDROID_EMU_read_color_buffer_dma",
-                            "ANDROID_EMU_hwc_multi_configs",
-                            "GL_OES_EGL_image_external_essl3",
-                            "GL_OES_vertex_array_object",
-                            "GL_KHR_texture_compression_astc_ldr",
-                            "ANDROID_EMU_host_side_tracing",
-                            "ANDROID_EMU_gles_max_version_3_0",
-                        ),
-                    )
-                    .setScreenDensity(420)
-                    .setSdkVersion(35)
-                    .setCodename("REL")
-                    .setSdkRuntime(Devices.SdkRuntime.newBuilder().setSupported(true))
-                    .setRamBytes(2067869696)
-                    .setBuildBrand("google")
-                    .setBuildDevice("emu64xa")
-                    .setSocManufacturer("AOSP")
-                    .setSocModel("ranchu")
-                    .build()
-            )
+        private val validDeviceAttributes = javaClass.classLoader
+            .getResourceAsStream("valid-device-attributes.txtpb")!!
+            .use {
+                val builder = DeviceAttributes.newBuilder()
+                it.reader().use { TextFormat.merge(it, builder) }
+                builder
+            }
             .build()
 
         private val validGetAppListingRequest: GetAppListingRequest = GetAppListingRequest.newBuilder()
