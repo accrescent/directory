cf8c58d955cab2a83cf0d8d1ac7a97ab7494ead9
lberrymage <logan@lberrymage.dev>
2025-03-27 11:09:29 -0700

    Change country_code field type to FixedString(2)
    Country codes as we use them are ISO 3166 alpha-2 codes, which are
    always exactly 2 characters long, so FixedString(2) is probably slightly
    more efficient than LowCardinality(String).

--- directory/src/main/resources/create-events-tables.sql
+++ directory/src/main/resources/create-events-tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE downloads (
     app_id String,
     version_code UInt32,
     device_sdk_version UInt16,
-    country_code LowCardinality(String),
+    country_code FixedString(2),
 )
 ENGINE = MergeTree
 ORDER BY (app_id, date, version_code, country_code);
@@ -17,7 +17,7 @@ CREATE TABLE listing_views (
     app_id String,
     language_code LowCardinality(String),
     device_sdk_version UInt16,
-    country_code LowCardinality(String),
+    country_code FixedString(2),
 )
 ENGINE = MergeTree
 ORDER BY (app_id, date, language_code, country_code);
