adec0e907b19105c27ff0cbfe807593a15f05606
lberrymage <logan@lberrymage.dev>
2025-08-25 16:57:12 +0000

    Consistently use lowercase for SQL type names
    Column nmaes were moved around as the Hibernate baseline was
    re-generated with a newer version of Hibernate for slightly more
    readable column ordering.

--- directory/src/main/resources/db/migration/V1__Add_Hibernate_baseline.sql
+++ directory/src/main/resources/db/migration/V1__Add_Hibernate_baseline.sql
@@ -1,40 +1,40 @@
 CREATE SEQUENCE images_seq START WITH 1 INCREMENT BY 50;
 
 CREATE TABLE apks (
-    id TEXT NOT NULL,
+    id text NOT NULL,
     release_channel_id uuid NOT NULL,
-    uncompressed_size INTEGER NOT NULL,
+    uncompressed_size integer NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE apps (
-    default_listing_language TEXT NOT NULL,
     id text NOT NULL,
+    default_listing_language text NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE images (
-    id BIGINT NOT NULL,
-    object_id TEXT NOT NULL,
+    id bigint NOT NULL,
+    object_id text NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE listings (
-    icon_image_id BIGINT NOT NULL,
-    app_id TEXT NOT NULL,
-    language TEXT NOT NULL,
-    name TEXT NOT NULL,
-    short_description TEXT NOT NULL,
+    app_id text NOT NULL,
+    language text NOT NULL,
+    name text NOT NULL,
+    short_description text NOT NULL,
+    icon_image_id bigint NOT NULL,
     PRIMARY KEY (app_id, language)
 );
 
 CREATE TABLE release_channels (
-    version_code INTEGER NOT NULL,
-    id UUID NOT NULL,
-    app_id TEXT NOT NULL,
-    name TEXT NOT NULL,
-    version_name TEXT NOT NULL,
-    build_apks_result BYTEA NOT NULL,
+    id uuid NOT NULL,
+    app_id text NOT NULL,
+    build_apks_result bytea NOT NULL,
+    name text NOT NULL,
+    version_code integer NOT NULL,
+    version_name text NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (app_id, name)
 );
