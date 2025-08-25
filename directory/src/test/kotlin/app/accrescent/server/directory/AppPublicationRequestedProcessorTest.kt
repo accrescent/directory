3680b0cc4e637e3aaf06779d1b7bf350e49f211e
lberrymage <logan@lberrymage.dev>
2025-08-25 21:39:11 +0000

    Reset database between AppPublicationRequestedProcessor tests

--- directory/src/test/kotlin/app/accrescent/server/directory/AppPublicationRequestedProcessorTest.kt
+++ directory/src/test/kotlin/app/accrescent/server/directory/AppPublicationRequestedProcessorTest.kt
@@ -4,20 +4,41 @@
 
 package app.accrescent.server.directory
 
+import app.accrescent.server.directory.data.AppRepository
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction
 import io.quarkus.test.common.QuarkusTestResource
+import io.quarkus.test.hibernate.reactive.panache.TransactionalUniAsserter
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.kafka.InjectKafkaCompanion
 import io.quarkus.test.kafka.KafkaCompanionResource
+import io.quarkus.test.vertx.RunOnVertxContext
+import io.smallrye.mutiny.Uni
 import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.function.Supplier
 
 @QuarkusTest
 @QuarkusTestResource(KafkaCompanionResource::class)
 class AppPublicationRequestedProcessorTest {
     @InjectKafkaCompanion
     lateinit var kafka: KafkaCompanion
+
+    @Inject
+    private lateinit var appRepository: AppRepository
+
+    @BeforeEach
+    @RunOnVertxContext
+    fun cleanDatabase(asserter: TransactionalUniAsserter) {
+        asserter.execute(Supplier { cleanDatabase() })
+    }
+
+    @WithTransaction
+    fun cleanDatabase(): Uni<Long> {
+        return appRepository.deleteAll()
+    }
 
     @BeforeEach
     fun registerKafkaSerdes() {
